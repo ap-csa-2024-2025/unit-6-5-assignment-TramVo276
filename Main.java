0a93f50b2bbd5e4d35e8e6e728bd6ee9ded52c2e
TramVo276 <tvo941352@gmail>
2025-02-07 09:58:51 -0800

    ghuv

--- Main.java
+++ Main.java
@@ -4,12 +4,38 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // Create some arrays here to test your methods
-    
+    int size;
+    String[] words;
+    Scanner sc = new Scanner(System.in);
+    System.out.println("How many words do you want to enter?")
+    size = sc.nextInt();
+    words = new String[size]; //make the array
+
+    sc.nextLine();  //consumes the enter key left behind by nextInt
+    System.out.println("Enter" + size + "words");
+    for (int i = 0; i< size; i++)
+    {
+      words[i] = sc.nextLine(); //fill the array with user input
+    }
+    System.out.println("Hello World")    
   }
 
   public static void reverse(String[] arr)
   {
+    for (String word : arr)
+    {
+      System.out.println(reverseWord);
+    }
+  }
+  public static String reverseWord(String Word)
+    {
+      String rev = "";
+      for (int i = word.length()-1; i >= 0; i--)
+      {
+        rev += word.substring(i, i+1);
+      }
+      return rev;
+    }
 
   }
 
@@ -22,4 +48,5 @@ public class Main
   {
     return 0.0;
   }
+  
 }
